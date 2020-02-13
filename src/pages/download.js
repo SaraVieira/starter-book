7e64b1fe32676c6a5bfe4f1590f415f9b781829c
Sara <hey@iamsaravieira.com>
2020-02-13 22:56:11 +0100

    fix build

--- src/pages/download.js
+++ src/pages/download.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const DownloadPage = ({ location }) => {
   const user = (location.state || {}).email;
-  if (!user) document.location.href = "/";
+  if (!user || !document) document.location.href = "/";
   if (user) {
     return <h1>Download the book</h1>;
   }
